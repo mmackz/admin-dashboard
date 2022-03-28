b00622ed2a41fec5b9b207c71899fb9dea604a8a
Mmackz <matthewmackenz@gmail.com>
2022-03-28 13:18:54 -0700

    ✨ add button to toggle sidebar

---
+++ index.js
@@ -0,0 +1,47 @@
+const menu = document.querySelector(".hamburger");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+// opens sidebar when hamburger menu is clicked
+menu.addEventListener("click", (event) => {
+   event.stopPropagation();
+   sidebar.classList.toggle("open");
+   document.body.style.overflow = "hidden";
+   setSidebarHeight();
+});
+
+// stop event from bubbling up to the body
+sidebar.addEventListener("click", (event) => {
+   event.stopPropagation();
+});
+
+// closes sidebar when body outside sidebar is clicked
+document.body.addEventListener("click", () => {
+   if (sidebar.classList.contains("open")) {
+      sidebar.classList.remove("open");
+      document.body.style = "";
+   }
+});
+
+// closes sidebar when "X" button is clicked
+closeBtn.addEventListener("click", () => {
+   if (sidebar.classList.contains("open")) {
+      sidebar.classList.remove("open");
+      document.body.style = "";
+   }
+});
+
+// sets sidebar to full size of content on short screens
+window.addEventListener("resize", () => {
+   setSidebarHeight();
+});
+
+function setSidebarHeight() {
+   const size = document.body.getBoundingClientRect();
+   const windowHeight = window.innerHeight;
+   if (size.width <= 740 && windowHeight <= 610) {
+      sidebar.style.minHeight = windowHeight + "px";
+   } else {
+      sidebar.style = "";
+   }
+};
